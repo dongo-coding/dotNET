f7e84cfb67610f88fb7d5086ca3b04883a02bf48
Do Ngo <bahuynh619@gmail.com>
2025-06-27 08:58:01 +0700

    Create cta.cs

---
+++ cta.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace String {
+    class Program {
+        static void Main(string[] args) {
+            string s = Console.ReadLine();
+            char c = char.Parse(Console.ReadLine());
+            int cnt=0;
+            for(int i=0;i<s.Length;i++){
+                if(s[i]==c){
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
