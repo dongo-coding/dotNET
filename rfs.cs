ca231ca277fdd4690ed4a67697cb9f9f07ee8353
Do Ngo <bahuynh619@gmail.com>
2025-06-27 09:39:12 +0700

    Create rfs.cs

---
+++ rfs.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace String {
+    class Program {
+        static void Main(string[] args) {
+			string s = Console.ReadLine();
+			for(int i=s.Length - 1;i>=0;i--){
+                Console.Write(s[i]);          //đảo ngược xâu ban đầu
+            }
+		}
+    }
+}
